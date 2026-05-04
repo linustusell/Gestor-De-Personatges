7398b16d860677940de3e14d6a279c4240c21ca9
Primera Prova <19ltusell@iesperealsius.cat>
2026-05-04 15:05:30 +0200

    Arreglo Alguns errors de llista de Personatges

--- src/main/java/com/tusell/model/LlistaPersonatges.java
+++ src/main/java/com/tusell/model/LlistaPersonatges.java
@@ -3,18 +3,19 @@ package com.tusell.model;
 import java.util.ArrayList;
 
 public class LlistaPersonatges {
-    private ArrayList<Personatge> personatges;
+    private ArrayList<Personatge> personatges = new ArrayList<>();
 
     public LlistaPersonatges(){
-
     }
 
     public ArrayList<Personatge> getPersonatges() {
         return personatges;
     }
+
     public void setPersonatges(ArrayList<Personatge> personatges) {
         this.personatges = personatges;
     }
+
     public void AfegirPersonatge(Personatge p){
         personatges.add(p);
     }
